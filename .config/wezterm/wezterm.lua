8e073223a472d378779abec88a0c4c299c4df7c9
LorNtz <lortz@foxmail.com>
2023-12-11 00:05:24 +0800

    wezterm appearence adjustment

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,16 +6,47 @@ local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- Settings
-config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Atelier Lakeside (base16)"
+config.color_scheme = "Ayu Mirage"
 config.font = wezterm.font_with_fallback({
   { family = "CaskaydiaCove Nerd Font",  scale = 1.2 },
   { family = "FantasqueSansM Nerd Font", scale = 1.2 },
 })
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  -- font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 20.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  -- active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  -- inactive_titlebar_bg = '#333333',
+}
+
+config.window_padding = {
+  left = '2cell',
+  right = '2cell',
+  top = '1cell',
+  bottom = '0.5cell',
+}
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
